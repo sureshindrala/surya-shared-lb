c4b99b44920ae669dd5f8f481344c8953cac0da8
Sumanth3420 <indralasumanth3420@gmail.com>
2024-06-19 20:12:35 +0530

    def

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -204,9 +204,7 @@ def call (Map pipelineParams){
             }
         }
     }
-    
-    }
-    def dockerBuildandPush() {
+      def dockerBuildandPush() {
         return {
                 echo "******************************** Build Docker Image ********************************"
                 sh "cp ${workspace}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
@@ -219,6 +217,8 @@ def call (Map pipelineParams){
                 echo "Pushed the image succesfully!!!"
         }
     }
+ }
+  
 
     def dockerDeploy(envDeploy, hostPort, contPort) {
         return {
