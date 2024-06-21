e488dfc069970789a87fc447de2b80f3aa4c68c4
Sumanth3420 <indralasumanth3420@gmail.com>
2024-06-21 18:53:05 +0530

    added

--- src/com/i27academy/builds/K8s.groovy
+++ src/com/i27academy/builds/K8s.groovy
@@ -9,7 +9,7 @@ class k8s {
     def auth_login(gke_cluster_name, gke_zone, gke_project) {
         jenkins.sh """#!/bin/bash
         echo "Entering Authentication method for GKE cluster login"
-        gcloud config set account jenkins@chromatic-craft-424811-h4.iam.gserviceaccount.com
+        #gcloud config set account jenkins@chromatic-craft-424811-h4.iam.gserviceaccount.com
         gcloud compute instances list
         echo "******** Listing number of nodes in K8S *********"
         kubectl get nodes
