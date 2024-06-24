5e30e7485db8ce7e0afd1f6ce1b4906db7536ca6
Sumanth3420 <indralasumanth3420@gmail.com>
2024-06-24 17:55:58 +0530

    ...

--- src/com/i27academy/builds/K8s.groovy
+++ src/com/i27academy/builds/K8s.groovy
@@ -6,10 +6,10 @@ class K8s {
         this.jenkins = jenkins
     }
     def auth_login () {
-        jenkins.sh """#!/bin/bash
+        jenkins.sh '''#!/bin/bash
         echo "Authenticating method for cluster login"
         gcloud config set account jenkins@chromatic-craft-424811-h4.iam.gserviceaccount.com
         gcloud compute instances list
-        """
+        '''
     }
 }
