fcb0b54647f11164faabcd4d1f511cf6d2882ae9
Sumanth3420 <indralasumanth3420@gmail.com>
2024-06-25 19:58:20 +0530

    dfdf

--- src/com/i27academy/builds/vars/dockerPipeline.groovy
+++ src/com/i27academy/builds/vars/dockerPipeline.groovy
@@ -213,18 +213,18 @@ def call (Map pipelineParams) {
     }
 }
 
-// This method will build image and push to registry
+
 def dockerBuildandPush(){
     return {
-            echo "******************************** Build Docker Image ********************************"
-            sh "cp ${workspace}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
-            sh "ls -la ./.cicd"
-            sh "docker build --force-rm --no-cache --pull --rm=true --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
-            echo "******************************** Login to Docker Repo ********************************"
-            sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
-            echo "******************************** Docker Push ********************************"
-            sh "docker push ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
-            echo "Pushed the image succesfully!!!"
+        echo "******************************** Build Docker Image ********************************"
+        sh "cp ${workspace}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
+        sh "ls -la ./.cicd"
+        sh "docker build --force-rm --no-cache --pull --rm=true --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
+        echo "******************************** Login to Docker Repo ********************************"
+        sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
+        echo "******************************** Docker Push ********************************"
+        sh "docker push ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+        echo "Pushed the image succesfully!!!"
     }
 }
 
@@ -277,12 +277,4 @@ def imageValidation() {
     }
 }
 
-
-def buildApp() {
-    return {    
-        echo "Building the ${env.APPLICATION_NAME} application"
-        sh "mvn clean package -DskipTests=true"
-    }
-
-}
 }
