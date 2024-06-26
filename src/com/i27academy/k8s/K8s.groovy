4a9f1f108e2adbaac2792ab024f1026acce086f2
Sumanth3420 <indralasumanth3420@gmail.com>
2024-06-26 10:47:04 +0530

    k8s pipeline created

---
+++ src/com/i27academy/k8s/K8s.groovy
@@ -0,0 +1,15 @@
+package com.i27academy.k8s
+
+class K8s{
+    def jenkins
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+    def auth_login() {
+        jenkins.sh """#!/bin/bash
+        echo "***********Entering in The GKE cluster*******"
+        gcloud config set account jenkins@chromatic-craft-424811-h4.iam.gserviceaccount.com 
+        gcloud compute list
+        """
+    }
+}
